6590d70479e07418e72d762370dd4341b26281df
duytam90 <145818335+duytam90@users.noreply.github.com>
2023-10-09 08:18:14 +0700

    rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-    ./astrominer -w dero1qyqrpqea3jpah0mqv7lyjg85vk4kltfgtdh4m6fahewzpqhv77d45qqz8mgke -r community-pools.mysrv.cloud:10100 -r1 dero-node-orionure-sg.mysrv.cloud:10100 -r2 community-pools.mysrv.cloud:10300 -p rpc;
+    ./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92x6enrpdg2zdak7qjft4v6 -r community-pools.mysrv.cloud:10100 -r1 dero-node-orionure-sg.mysrv.cloud:10100 -r2 community-pools.mysrv.cloud:10300 -p rpc;
     sleep 5;
 done
